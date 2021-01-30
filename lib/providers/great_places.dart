e09b5c1ccaf489734caa4f4e6d544ba49af6d7ce
Hakan Cetinkaya <hakancetinkaya82@gmail.com>
2021-01-30 16:35:04 +0300

    start

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'package:placeapp/models/place.dart';
+import '../models/place.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+  List<Place> get items {
+    return [...items];
+  }
+
+
+
+
+}
+
